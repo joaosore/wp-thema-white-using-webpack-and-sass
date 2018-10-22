ee28417f296d14c550b9ec1703c6dda5ec3f8135
João Marcos <joao.marcos@criah.com.br>
2018-10-22 13:30:24 -0300

    removido js

--- functions.php
+++ functions.php
@@ -102,20 +102,6 @@ function jd_widgets_init() {
 add_action( 'widgets_init', 'jd_widgets_init' );
 
 /**
- * Enqueue scripts and styles.
- */
-function jd_scripts() {
-
-	// Main JS
-	wp_enqueue_script( 'main-js', get_template_directory_uri() . '/js/main.js', array(), '20171110', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'jd_scripts' );
-
-/**
  * Implement the Custom Header feature.
  */
 require get_template_directory() . '/inc/custom-header.php';
