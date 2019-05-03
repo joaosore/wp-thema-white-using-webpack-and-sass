2214f286a679f059232ab7f7c1238f46affe50fa
João Marcos <joao.marcos@criah.com.br>
2019-05-02 22:57:00 -0300

    Update site

--- src/js/modulos/loader.js
+++ src/js/modulos/loader.js
@@ -1,3 +1,3 @@
 console.log('');
-console.log('%c WP Theme by https://johnnydev.com', 'background-color: #24292E; color: #fff; padding: 6px;');
+console.log('%c WP Theme by https://www.johnnydev.com.br', 'background-color: #24292E; color: #fff; padding: 6px;');
 console.log('');
